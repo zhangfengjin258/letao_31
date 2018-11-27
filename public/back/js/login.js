5dd518f1ef0ade8730ee188319f28093708684c7
zhangfengjin258 <1029221382@qq.com>
2018-11-27 21:05:46 +0800

    登录页面完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -28,6 +28,9 @@ $(function(){
                         min:2,
                         max:6,
                         message:"用户名长度必须是2-6位"
+                    },
+                    callback:{
+                        message:"用户名不存在",
                     }
                 }
             },
@@ -44,6 +47,9 @@ $(function(){
                         min:6,
                         max:12,
                         message:"密码长度必须是6-12位"
+                    },
+                    callback:{
+                        message:"密码错误",
                     }
                 }
             }
@@ -61,15 +67,25 @@ $(function(){
             url:'/employee/employeeLogin',
             // 表单序列化获取username与password
             data:$('#form').serialize(),
-            dataType:'json',
+
             success:function(info){
                 // console.log(info);
                 if(info.error === 1000){
-                    alert('用户名不存在');
+                    // alert('用户名不存在');
+                    /**
+                     *更新当前input校验状态，更新成失败
+                     *updateStatus
+                     * 参数1：field 字段名称
+                     * 参数2：status 状态
+                     *      NOT_VALIDATED(未校验),VALIDATING(校验中),INVALID(校验失败),VALID(校验成功)
+                     * 参数3：validator 配置校验规则【用来配置输出的提示信息】
+                     */
+                    $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback');
                     return;
                 }
                 if(info.error === 1001){
-                    alert('密码错误');
+                    // alert('密码错误');
+                    $('#form').data('bootstrapValidator').updateStatus('password','INVALID','callback');
                     return;
                 }
                 if(info.success){
