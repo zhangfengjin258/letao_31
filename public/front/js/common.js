a2b8ed7260ac540b57b985e7d818f883fd5f7284
zhangfengjin258 <1029221382@qq.com>
2018-12-02 20:36:19 +0800

    移动端主页完成

--- public/front/js/common.js
+++ public/front/js/common.js
@@ -1,7 +1,8 @@
     // 区域滚动
-    // mui('.mui-scroll-wrapper').scroll({
-    //     deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-    // });
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005 ,//flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+        indicators: false,//去除区域滚动滑动条
+    });
 
     //获得slider插件对象
     var gallery = mui('.mui-slider');
